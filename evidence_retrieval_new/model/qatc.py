ab92ab98b827d9148854af39086bfae7311bdf9f
xndien2004 <xuandienk4@gmail.com>
2025-02-22 22:06:45 +0700

    update

--- evidence_retrieval_new/model/qatc.py
+++ evidence_retrieval_new/model/qatc.py
@@ -24,38 +24,6 @@ class Rational_Tagging(nn.Module):
         p = torch.sigmoid(self.fc(h_out))  
         return p
     
-class TextEncoder(nn.Module):
-    def __init__(self, dim):
-        super(TextEncoder, self).__init__()
-        self.global_proj = nn.Linear(dim, dim)
-        self.local_proj = nn.Linear(dim, dim)
-        self.qa_proj = nn.Linear(dim, dim)
-
-    def forward(self, hidden_states):        
-        # Tạo 3 dạng representation khác nhau
-        global_view = self.global_proj(hidden_states)  # Toàn bộ văn bản
-        local_view = self.local_proj(hidden_states[:, :256, :])  # Phần đầu tiên
-        qa_view = self.qa_proj(hidden_states)  # Thông tin tập trung vào câu hỏi
-        
-        return global_view, local_view, qa_view
-
-
-class GatedAttention(nn.Module):
-    def __init__(self, hidden_size):
-        super(GatedAttention, self).__init__()
-        self.gate = nn.Linear(hidden_size * 3, hidden_size)
-        self.softmax = nn.Softmax(dim=-1)
-
-    def forward(self, global_view, local_view, qa_view):
-        combined = torch.cat([global_view, local_view, qa_view], dim=-1)
-        gate_values = self.softmax(self.gate(combined))
-        
-        fused_representation = gate_values[:, 0:1] * global_view + \
-                               gate_values[:, 1:2] * local_view + \
-                               gate_values[:, 2:3] * qa_view
-        return fused_representation
-
-
 class QATC(nn.Module):
     def __init__(self, config):
         super(QATC, self).__init__()
@@ -83,8 +51,6 @@ class QATC(nn.Module):
             self.dropout,
             nn.Linear(self.model.config.hidden_size, 2),
         )
-        self.encoder = TextEncoder(self.model.config.hidden_size)
-        self.gated_attention = GatedAttention(self.model.config.hidden_size)
         self.tagging = Rational_Tagging(self.model.config.hidden_size) 
         self.model.pooler = None
     def forward(self, input_ids, attention_mask):
@@ -93,10 +59,7 @@ class QATC(nn.Module):
 
         pt = self.tagging(qa_ouputs)
 
-        global_view, local_view, qa_view = self.encoder(qa_ouputs)
-        fused_representation = self.gated_attention(global_view, local_view, qa_view)
-
-        logits = self.qa_outputs(fused_representation) 
+        logits = self.qa_outputs(qa_ouputs) 
         start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1).contiguous()
         end_logits = end_logits.squeeze(-1).contiguous()
