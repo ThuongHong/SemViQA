cf66f8c6019c08c1d54b88db4c7ff5a8f6715c79
xndien2004 <xuandienk4@gmail.com>
2025-02-22 23:32:13 +0700

    update

--- claim_verification/model.py
+++ claim_verification/model.py
@@ -1,27 +1,27 @@
 import torch.nn as nn
 from transformers import AutoModel, XLMRobertaModel
 
+class AttentionPooling(nn.Module):
+    def __init__(self, hidden_size):
+        super(AttentionPooling, self).__init__()
+        self.attn = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        attn_weights = torch.softmax(self.attn(x), dim=1)  # Tính attention weights
+        pooled_output = torch.sum(attn_weights * x, dim=1)  # Weighted sum của tất cả token
+        return pooled_output
+
 class ClaimVerification(nn.Module):
-    def __init__(self, n_classes, 
-                 name_model):
+    def __init__(self, name_model):
         super(ClaimVerification, self).__init__()
-        if 'infoxlm' in name_model or 'xlm-roberta' in name_model:
-            self.bert = XLMRobertaModel.from_pretrained(name_model) 
-        else:
-            self.bert = AutoModel.from_pretrained(name_model)
-        # self.norm = nn.LayerNorm(self.bert.config.hidden_size)
+        self.bert = AutoModel.from_pretrained(name_model)
+        self.attn_pooling = AttentionPooling(self.bert.config.hidden_size)
         self.drop = nn.Dropout(p=0.3)
-        self.fc = nn.Linear(self.bert.config.hidden_size, n_classes)
-        nn.init.normal_(self.fc.weight, std=0.02)
-        nn.init.normal_(self.fc.bias, 0)
+        self.fc = nn.Linear(self.bert.config.hidden_size, 3)
 
     def forward(self, input_ids, attention_mask):
-        _, output = self.bert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            return_dict=False # Dropout will errors if without this
-        )
-        # x = self.norm(output)
-        x = self.drop(output)
-        x = self.fc(x)
-        return x
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, return_dict=True)
+        pooled_output = self.attn_pooling(outputs.last_hidden_state)
+        logits = self.fc(self.drop(pooled_output))
+        return logits
+
