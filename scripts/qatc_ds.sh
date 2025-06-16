8ed5c0eeef3a2cf6413d85a50295706f6c22987d
Nguyễn Văn Nam <97231719+DAVID-NGUYEN-S16@users.noreply.github.com>
2025-06-16 23:35:16 +0700

    Update qatc_ds.sh

--- scripts/qatc_ds.sh
+++ scripts/qatc_ds.sh
@@ -1,44 +1,37 @@
+#!/usr/bin/env bash
 nvidia-smi
+pip install deepspeed
 source ~/.bashrc
 
 # Log GPU utilization in background
 while true; do nvidia-smi > gpu_status.txt; sleep 5; done &
 
-# Activate conda environment (edit if different)
-module load shared conda
-. $CONDAINIT
-conda activate sem_deep
-
-conda info --envs
-echo "Conda Environment: $CONDA_DEFAULT_ENV"
-
-export PYTHONPATH="SemViQA:$PYTHONPATH"
-
+export PYTHONPATH="/kaggle/working/SemViQA:$PYTHONPATH"
+cd /kaggle/working/SemViQA
 # ---------------------------
 # Training parameters
 # ---------------------------
-BS=12            # Effective per-device batch size
+BS=6            # Effective per-device batch size
 EPOCHS=16        # Number of training epochs
 LR=2e-6          # Learning rate
 MODEL_NAME="microsoft/infoxlm-large"  # Base model (change if desired)
 OUTPUT_DIR="./output_deepspeed"
-DS_CONFIG="SemViQA/semviqa/ser/ds_zero2.json"
+DS_CONFIG="./semviqa/ser/ds_zero2.json"
 
 # Launch training with 🤗 Accelerate + DeepSpeed (Stage-2)
-accelerate launch \
-    --multi_gpu \
-    --num_processes $(nvidia-smi -L | wc -l) \
-    --module semviqa.ser.main \
-    # SemViQA/semviqa/ser/main.py \
-        --model_name "$MODEL_NAME" \
-        --output_dir "$OUTPUT_DIR" \
-        --train_batch_size $BS \
-        --num_train_epochs $EPOCHS \
-        --learning_rate $LR \
-        --train_data "train.csv" \
-        --eval_data "test.csv" \
-        --patience 5 \
-        --is_pretrained 0 \
+accelerate launch \\
+    --multi_gpu \\
+    --num_processes $(nvidia-smi -L | wc -l) \\
+    ./semviqa/ser/main.py \\
+        --model_name "microsoft/infoxlm-large" \\
+        --output_dir "$OUTPUT_DIR" \\
+        --train_batch_size $BS \\
+        --num_train_epochs $EPOCHS \\
+        --learning_rate $LR \\
+        --train_data "/kaggle/input/semviqa-data/data/evi/viwiki_train.csv" \\
+        --eval_data "/kaggle/input/semviqa-data/data/evi/viwiki_test.csv" \\
+        --patience 5 \\
+        --is_pretrained 0 \\
         --ds_config "$DS_CONFIG"
 
-echo "DeepSpeed QATC training completed!" 
+echo "DeepSpeed QATC training completed!"
